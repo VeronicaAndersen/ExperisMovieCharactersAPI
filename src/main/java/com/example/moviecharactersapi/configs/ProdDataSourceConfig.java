f493e126caedd91408aea2e6323dfd64c541df18
Veronica Andersen <veronica.andersen@se.experis.com>
2022-08-26 16:56:10 +0200

    Feat: Added config and properties.

---
+++ src/main/java/com/example/moviecharactersapi/configs/ProdDataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.example.moviecharactersapi.configs;
+
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("prod")
+public class ProdDataSourceConfig {
+  @Bean
+  public DataSource dataSource() throws URISyntaxException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.url(dbUrl);
+    dataSourceBuilder.username(username);
+    dataSourceBuilder.password(password);
+    return dataSourceBuilder.build();
+  }
+}
